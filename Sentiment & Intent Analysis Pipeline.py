bd3bdaec5bbd9001262dc7c0a1175dc16823a1a7
Vaibhav Chaturvedi <110451139+vaihbav11@users.noreply.github.com>
2026-06-02 23:59:36 +0530

    Update Sentiment & Intent Analysis Pipeline.py

--- Sentiment & Intent Analysis Pipeline.py
+++ Sentiment & Intent Analysis Pipeline.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import (accuracy_score, precision_score, recall_score,
                               f1_score, confusion_matrix, ConfusionMatrixDisplay,
